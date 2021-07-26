9e04d4eb5b20a69639d412989afd4f242d1006ff
vsimakova <67300899+vsimakova@users.noreply.github.com>
2021-07-27 00:22:36 +0300

    initial  commit

---
+++ src/com/coderscampus/Assignment2Application.java
@@ -0,0 +1,40 @@
+package com.coderscampus;
+import java.util.Scanner;
+public class Assignment2Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int answer = 1 + (int)(Math.random() * 100);
+        Scanner sc = new Scanner(System.in);
+        boolean flag = false;
+        System.out.println("Pick a number between 1 and 100");
+        for (int i=0; i<5; i++) {
+            if (sc.hasNextInt()) {
+                int guess = sc.nextInt();
+                if (guess < answer && guess >= 1 && guess <= 100 && i != 4) {
+                    System.out.println("Plese pick a higher number");
+                }
+                if (guess < 1 || guess > 100) {
+                    System.out.println("Your guess is not between 1 and 100, please try again");
+                    i--;
+                } 
+                if (guess > answer && guess >= 1 && guess <= 100 && i != 4) {
+                    System.out.println("Please pick a lower number");
+                }
+                if (guess == answer) {
+                    System.out.println("You win!");
+                    flag = true;
+                }
+            } else {
+                System.out.println("incorrect input, try again");
+                i--;
+                sc.next();
+            }
+        }
+        if (!flag) {
+            System.out.print("You lose, the number to guess was " + answer);
+        }
+        sc.close();
+	}
+
+}
